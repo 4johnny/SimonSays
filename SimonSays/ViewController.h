26bf2420bdebda455a01b4dafd4c3cb7937de4b8
4johnny <4johnny@users.noreply.github.com>
2015-01-23 16:07:21 -0800

    Initial Commit

---
+++ SimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimonSays
+//
+//  Created by Johnny on 2015-01-23.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
